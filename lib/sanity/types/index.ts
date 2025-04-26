b3454c0ac9aa3014babdd84c5e5b53fcef51826c
Das <saikumar040060@gmail.com>
2025-04-26 23:35:02 +0530

    fix: remove wrong ApiResponse types

--- lib/sanity/types/index.ts
+++ lib/sanity/types/index.ts
@@ -1,10 +1,6 @@
 export * from './menu';
 export * from './shared';
 
-// You can also export combined types here
-export type ApiMenuResponse = ApiResponse<MenuItem[]>;
-export type ApiCategoryResponse = ApiResponse<Category[]>;
-
 // Utility types
 export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
 export type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = {
